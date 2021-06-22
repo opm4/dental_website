aab43553998c41e54732f0a774d6f25eb757d9eb
OPM4 <mateioprea@gmail.com>
2021-06-23 01:14:21 +0300

    updated app module import to lower level

--- dental_website/dental_website/settings.py
+++ dental_website/dental_website/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
+    'dental_website.website',
     'gunicorn',
 ]
 
