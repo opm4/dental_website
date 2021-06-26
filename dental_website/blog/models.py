0dc8e7e2193e5e2d50b32b691d338161da5b2890
Matei O <mateioprea@gmail.com>
2021-06-26 18:39:53 +0300

    Add Blog Post Page

--- dental_website/blog/models.py
+++ dental_website/blog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default='My Blog Article')
+    title_tag = models.CharField(max_length=255) #, default='My Blog Article'
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     
     
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+    
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"pk": self.pk})
+    
