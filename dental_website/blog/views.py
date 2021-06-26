78ec368e1ca92aceb8eb5f74e80c2bdb41f5ac88
Matei O <mateioprea@gmail.com>
2021-06-26 17:14:19 +0300

    class based views for list and details

--- dental_website/blog/views.py
+++ dental_website/blog/views.py
@@ -1,5 +1,17 @@
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
-def home(request):
-    return render(request, 'home_blog.html', {})
+# def home(request):
+#     return render(request, 'home_blog.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home_blog.html'
+    
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details_blog.html'
+    
