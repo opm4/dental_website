a46d8a86356166d0eea6efaa58330d8a1b3cb455
Matei O <mateioprea@gmail.com>
2021-06-28 19:41:44 +0300

    Restrict Who Can Make Blog Posts

--- dental_website/blog/forms.py
+++ dental_website/blog/forms.py
@@ -23,7 +23,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is title placeholder text'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control','value':'', 'id':'author', 'type':'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             
