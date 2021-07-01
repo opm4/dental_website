59e6b89194a038d2b280e0807010041ea1a999e7
Matei O <mateioprea@gmail.com>
2021-07-01 21:29:05 +0300

    Edit Profile Page

--- dental_website/user_login/urls.py
+++ dental_website/user_login/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView
 # from . import views
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
+    path('user_profile/', UserEditView.as_view(), name="edit_profile"),
 ]
