229ffe4ac30f31c9d925c97accbd19ec70f27f7a
Matei O <mateioprea@gmail.com>
2021-07-03 20:38:44 +0300

    Comment Section

--- dental_website/blog/models.py
+++ dental_website/blog/models.py
@@ -52,3 +52,12 @@ class Profile(models.Model):
     
     def get_absolute_url(self):
         return reverse("show_profile_page", kwargs={"pk": self.pk})
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return '%s - %s' %(self.post.title, self.name)
