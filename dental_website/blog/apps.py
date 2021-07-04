b2f0674f506b5e3aac57bc28877f03d1538792e6
Matei O <mateioprea@gmail.com>
2021-07-04 15:02:11 +0300

    update apps config for Heroku

--- dental_website/blog/apps.py
+++ dental_website/blog/apps.py
@@ -1,5 +1,10 @@
+import os
 from django.apps import AppConfig
 
+if os.name == 'nt':
+        name_var = 'blog'
+else:
+        name_var = 'dental_website.blog'
 
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
--- dental_website/user_login/apps.py
+++ dental_website/user_login/apps.py
@@ -1,5 +1,10 @@
+import os
 from django.apps import AppConfig
 
+if os.name == 'nt':
+        name_var = 'user_login'
+else:
+        name_var = 'dental_website.user_login'
 
 class UserLoginConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
